cdea8ee1942392f99be3e0b8fe7085d46bf39c6c
adhelll <anton_01_99@mail.ru>
2021-02-06 15:36:33 +0300

    Track Version

---
+++ esaul_description/src/esaul_plugin.cpp
@@ -0,0 +1,82 @@
+#include <gazebo/gazebo.hh>
+#include <ros/ros.h>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include "geometry_msgs/Twist.h"
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/tfMessage.h>
+
+using namespace std;
+
+namespace gazebo
+{
+class ModelPush : public ModelPlugin
+{
+	public: 
+	ModelPush() : ModelPlugin()
+  	{
+		cout << "********************Plugin is creating********************" << endl;	
+  	}
+	void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+	{
+		this->model = _parent;
+		this->sdf = _sdf;
+		_joint_left= model->GetJoint("joint_left");
+		_joint_right = model->GetJoint("joint_right");
+		/*
+		wheel_joint_left_2 = model->GetJoint("joint_left_wheel_2");
+		wheel_joint_right_3 = model->GetJoint("joint_right_wheel_3");
+		wheel_joint_left_3 = model->GetJoint("joint_left_wheel_3");
+		wheel_joint_right_4 = model->GetJoint("joint_right_wheel_4");
+		wheel_joint_left_4 = model->GetJoint("joint_left_wheel_4");*/
+
+		if (sdf->HasElement("robotName"))
+		{
+			roboname = sdf->Get<std::string>("robotName");
+			cout << "********************Name received********************" << endl;
+			cout << "His name is " << roboname << endl;
+		}
+		else
+		{
+			cout << "********************Name don't received********************" << endl;
+		}
+		updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&ModelPush::OnUpdate, this));
+		nh = ros::NodeHandle(roboname);
+    		cmd_vel_sub_ = nh.subscribe("cmd_vel", 1, &ModelPush::CmdVel, this);
+	}
+	void OnUpdate()
+    	{	
+		_joint_right->SetVelocity(0, speed);
+		_joint_left->SetVelocity(0, -speed);
+		//i++;
+      		//wheel_joint_right_1->SetVelocity(0, lin_speed);
+		//wheel_joint_left_1->SetVelocity(0, lin_speed);
+		//const ignition::math::Vector3d contactWorldPosition(i, 0, 0);
+    	}
+	void CmdVel(const geometry_msgs::Twist& command)
+    	{
+		speed = command.linear.x;
+		
+      	}
+	~ModelPush()
+	{
+		nh.shutdown();
+	}
+	private: 
+	double speed=0;
+	//double lin_speed;
+	physics::ModelPtr model;
+	sdf::ElementPtr sdf;
+	std::string roboname;
+	//physics::ContactManager *contactManager;	
+    	ros::NodeHandle nh;
+	event::ConnectionPtr updateConnection;
+	ros::Subscriber cmd_vel_sub_;
+	physics::JointPtr _joint_left;
+	physics::JointPtr _joint_right;
+};
+  
+  GZ_REGISTER_MODEL_PLUGIN(ModelPush)
+}
