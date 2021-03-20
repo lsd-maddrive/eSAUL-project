ee7251ccadd0d0fb24d1aab53a19d08951fbba08
adhelll <anton_01_99@mail.ru>
2021-03-20 20:08:16 +0300

    gmapping

--- esaul_description/src/esaul_plugin.cpp
+++ esaul_description/src/esaul_plugin.cpp
@@ -15,6 +15,8 @@
 #include <realtime_tools/realtime_buffer.h>
 #include <realtime_tools/realtime_publisher.h>
 
+#define Pi 3.1415926535
+
 using namespace std;
 
 namespace gazebo
@@ -30,13 +32,10 @@ namespace gazebo
 			{
 				this->model = _parent;
 				this->sdf = _sdf;
-				_joint_left1= model->GetJoint("joint_left_wheel_1");
-				_joint_right1 = model->GetJoint("joint_right_wheel_1");
-				_joint_left2= model->GetJoint("joint_left_wheel_2");
-				_joint_right2 = model->GetJoint("joint_right_wheel_2");
-				_joint_left3= model->GetJoint("joint_left_wheel_3");
-				_joint_right3 = model->GetJoint("joint_right_wheel_3");
-				
+
+				_joint_left= model->GetJoint("joint_left_wheel");
+				_joint_right = model->GetJoint("joint_right_wheel");
+
 				sdf->GetElement("width_chassis")->GetValue()->Get(width);
 				sdf->GetElement("wh_rad")->GetValue()->Get(rad);
 
@@ -58,14 +57,43 @@ namespace gazebo
 			}
 		void updateOdom()
 			{
-				
+				delta_time = ros::Time::now().toSec()-delta_time;
+				if(delta_time>0)
+				{
+
+					left_turn = (_joint_left->Position(0))-left_turn;
+					right_turn = (_joint_right->Position(0))-right_turn;	
+
+					left_speed = (_joint_left->GetVelocity(0));
+					right_speed = (_joint_right->GetVelocity(0));	
+
+					left_speed = left_turn*rad/delta_time;
+					right_speed = right_turn*rad/delta_time;	
+
+					alpha = alpha + (right_speed-left_speed)/(width+rad)*delta_time;
+					x = x + (left_speed+right_speed)*0.5*cos(alpha)*delta_time;
+					y = y + (left_speed+right_speed)*0.5*sin(alpha)*delta_time;
+
+
+					if (abs(x-t_last)>0.1)
+						{
+							cout << "х" << x << endl;
+							cout << "y" << y << endl;
+							cout << "..........................." << endl;
+							t_last = x;
+						}
+				}
+				delta_time = ros::Time::now().toSec();
+				left_turn = (_joint_left->Position(0));
+				right_turn = (_joint_right->Position(0));		
 			}		
+			
 		void publishOdometryData()
 			{
 				nav_msgs::Odometry msg;
 				geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(alpha);
 				msg.header.stamp = ros::Time::now();
-				msg.header.frame_id = "map";
+				msg.header.frame_id = "odom";
 
 				msg.pose.pose.position.x = x;
 				msg.pose.pose.position.y = y;
@@ -80,8 +108,8 @@ namespace gazebo
 
 				geometry_msgs::TransformStamped odom_trans;
 				odom_trans.header.stamp = ros::Time::now();
-				odom_trans.header.frame_id = "map";
-				odom_trans.child_frame_id = "/eSAUl/odom";
+				odom_trans.header.frame_id = "odom";
+				odom_trans.child_frame_id = "base_link";
 
 				odom_trans.transform.translation.x = x;
 				odom_trans.transform.translation.y = y;
@@ -91,91 +119,63 @@ namespace gazebo
 				br->sendTransform(odom_trans);
 			}
 		void OnUpdate()
-			{	
+			{
 				updateOdom();
-				_joint_left1->SetVelocity(0, speed_l);
-				_joint_left2->SetVelocity(0, speed_l);
-				_joint_left3->SetVelocity(0, speed_l);
+				publishOdometryData();	
+				_joint_left->SetVelocity(0, speed_l);
+				_joint_right->SetVelocity(0, speed_r);
+					// if(abs(left_mid_speed-right_mid_speed)<0.1)
+					// {
+					// 	delta_time = ros::Time::now().toSec() - last_time;
+					// 	alpha = alpha;
+					// 	x = x + (left_mid_speed+right_mid_speed)*0.5*rad*cos(alpha)*delta_time;
+					// 	y = y + (left_mid_speed+right_mid_speed)*0.5*rad*sin(alpha)*delta_time;
+					// 	publishOdometryData();
+					// 	last_time = ros::Time::now().toSec();
+					// }
+					// if(abs(left_speed_1-left_speed_2)>1 || abs(left_speed_1-left_speed_3)>1)
+					// {
+					// 	delta_time = ros::Time::now().toSec() - last_time;
+					// 	alpha = alpha + rad*(left_mid_speed-right_mid_speed)/width*delta_time;
+					// 	x = x + (left_mid_speed+right_mid_speed)*0.5*rad*cos(alpha)*delta_time;
+					// 	y = y + (left_mid_speed+right_mid_speed)*0.5*rad*sin(alpha)*delta_time;
+					// 	// if(left_mid - right_mid<0.01)
+					// 	// {
+					// 	// 	x = rad*(left_mid + right_mid)/2;
+					// 	// 	y = rad*(left_mid + right_mid)/2;
+					// 	// }
+					// 	// if (abs(x-t_last)>0.01)
+					// 	// {
+					// 	// 	cout << rad*(left_mid_speed-right_mid_speed)/width << endl;
+					// 	// 	cout << alpha << endl;
+					// 	// 	cout << abs(left_speed_1-left_speed_2) << endl;
+					// 	// 	cout << abs(left_speed_1-left_speed_3) << endl;
+					// 	// 	// cout << right_speed_3 << endl;
+					// 	// 	cout << "..........................." << endl;
+					// 	// 	t_last = x;
+					// 	// }	
+					// 	//cout << "Зашел сюды" << endl;
+					// 	publishOdometryData();
+					// 	last_time = ros::Time::now().toSec();
+					// }
+					// delta_time = ros::Time::now().toSec() - last_time;
 
-				_joint_right1->SetVelocity(0, speed_r);
-				_joint_right2->SetVelocity(0, speed_r);
-				_joint_right3->SetVelocity(0, speed_r);				
+					// alpha = alpha + ang_speed*delta_time;
+					// //x = x+(speed_r*rad + speed_l*rad)*0.5*cos(alpha)*delta_time;
+					// y = y+(speed_r*rad + speed_l*rad)*0.5*sin(alpha)*delta_time;
 
-				left_turn_1 = (_joint_left1->Position(0));
-				left_turn_2 = (_joint_left2->Position(0));	
-				left_turn_3 = (_joint_left3->Position(0));	
-				right_turn_1 = (_joint_right1->Position(0));	
-				right_turn_2 = (_joint_right2->Position(0));	
-				right_turn_3 = (_joint_right3->Position(0));
-
-				left_speed_1 = (_joint_left1->GetVelocity(0));
-				left_speed_2 = (_joint_left2->GetVelocity(0));	
-				left_speed_3 = (_joint_left3->GetVelocity(0));	
-				right_speed_1 = (_joint_right1->GetVelocity(0));	
-				right_speed_2 = (_joint_right2->GetVelocity(0));	
-				right_speed_3 = (_joint_right3->GetVelocity(0));
-				left_mid = (left_turn_1 + left_turn_2 + left_turn_3)/3;
-				right_mid = (right_turn_1 + right_turn_2 + right_turn_3)/3;
-				left_mid_speed = (left_speed_1 + left_speed_2 + left_speed_3)/3;
-				right_mid_speed = (right_speed_1 + right_speed_2 + right_speed_3)/3;
-
-				cout << "Левое 1>>" << left_speed_1 << endl;
-				cout << "Левое 2>>" << left_speed_2 << endl;
-				cout << "Левое 3>>" << left_speed_3 << endl;
-				cout << "Правое 1>>" << right_speed_1 << endl;
-				cout << "Правое 2>>" << right_speed_2 << endl;
-				cout << "Правое 3>>" << right_speed_3 << endl;
-				// if(abs(left_mid_speed-right_mid_speed)<0.1)
-				// {
-				// 	delta_time = ros::Time::now().toSec() - last_time;
-				// 	alpha = alpha;
-				// 	x = x + (left_mid_speed+right_mid_speed)*0.5*rad*cos(alpha)*delta_time;
-				// 	y = y + (left_mid_speed+right_mid_speed)*0.5*rad*sin(alpha)*delta_time;
-				// 	publishOdometryData();
-				// 	last_time = ros::Time::now().toSec();
-				// }
-				// if(abs(left_speed_1-left_speed_2)>1 || abs(left_speed_1-left_speed_3)>1)
-				// {
-				// 	delta_time = ros::Time::now().toSec() - last_time;
-				// 	alpha = alpha + rad*(left_mid_speed-right_mid_speed)/width*delta_time;
-				// 	x = x + (left_mid_speed+right_mid_speed)*0.5*rad*cos(alpha)*delta_time;
-				// 	y = y + (left_mid_speed+right_mid_speed)*0.5*rad*sin(alpha)*delta_time;
-				// 	// if(left_mid - right_mid<0.01)
-				// 	// {
-				// 	// 	x = rad*(left_mid + right_mid)/2;
-				// 	// 	y = rad*(left_mid + right_mid)/2;
-				// 	// }
-				// 	// if (abs(x-t_last)>0.01)
-				// 	// {
-				// 	// 	cout << rad*(left_mid_speed-right_mid_speed)/width << endl;
-				// 	// 	cout << alpha << endl;
-				// 	// 	cout << abs(left_speed_1-left_speed_2) << endl;
-				// 	// 	cout << abs(left_speed_1-left_speed_3) << endl;
-				// 	// 	// cout << right_speed_3 << endl;
-				// 	// 	cout << "..........................." << endl;
-				// 	// 	t_last = x;
-				// 	// }	
-				// 	//cout << "Зашел сюды" << endl;
-				// 	publishOdometryData();
-				// 	last_time = ros::Time::now().toSec();
-				// }
-				// delta_time = ros::Time::now().toSec() - last_time;
-
-				// alpha = alpha + ang_speed*delta_time;
-				// //x = x+(speed_r*rad + speed_l*rad)*0.5*cos(alpha)*delta_time;
-				// y = y+(speed_r*rad + speed_l*rad)*0.5*sin(alpha)*delta_time;
-
-				// last_time = ros::Time::now().toSec();
-				// alpha_deg=alpha*180/3,1415926535;
-				// if((last_x!=x) || (last_y!=y) || (last_alpha!=alpha))
-				// {
-				// 	cout << "Позиция по Х: " << x << endl;
-				// 	cout << "Позиция по Y: " << y << endl;
-				// 	cout << "Угол градусы: " << alpha_deg << endl;
-				// 	last_x = x;
-				// 	last_y = y;
-				// 	last_alpha = alpha;
-				// }
+					// last_time = ros::Time::now().toSec();
+					// alpha_deg=alpha*180/3,1415926535;
+					// if((last_x!=x) || (last_y!=y) || (last_alpha!=alpha))
+					// {
+					// 	cout << "Позиция по Х: " << x << endl;
+					// 	cout << "Позиция по Y: " << y << endl;
+					// 	cout << "Угол градусы: " << alpha_deg << endl;
+					// 	last_x = x;
+					// 	last_y = y;
+					// 	last_alpha = alpha;
+					// }
+				
 			}
 			void CmdVel(const geometry_msgs::Twist &msg)
 			{
@@ -189,10 +189,8 @@ namespace gazebo
 				nh.shutdown();
 			}
 		private: 
-		double last_time;
-		double t_last, left_turn_1=0, left_turn_2=0, left_turn_3=0, right_turn_1=0, right_turn_2=0, right_turn_3=0;
-		double left_speed_1=0, left_speed_2=0, left_speed_3=0, right_speed_1=0, right_speed_2=0, right_speed_3=0;
-		double left_mid=0, right_mid=0, left_mid_speed=0, right_mid_speed=0;
+		double t_last, left_turn=0, right_turn=0;
+		double left_speed=0, right_speed=0;
 		double speed_r=0;
 		double speed_l=0;
 		double ang_speed=0, line_speed=0;
@@ -206,12 +204,8 @@ namespace gazebo
 		ros::NodeHandle nh;
 		event::ConnectionPtr updateConnection;
 		ros::Subscriber cmd_vel_sub_;
-		physics::JointPtr _joint_left1;
-		physics::JointPtr _joint_right1;
-		physics::JointPtr _joint_left2;
-		physics::JointPtr _joint_right2;
-		physics::JointPtr _joint_left3;
-		physics::JointPtr _joint_right3;
+		physics::JointPtr _joint_left;
+		physics::JointPtr _joint_right;
 		std_msgs::Float32 msg;
 		ros::Publisher odom_pub;
 		shared_ptr<tf::TransformBroadcaster> br;
